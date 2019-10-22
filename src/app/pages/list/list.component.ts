01c378acc4dbd2112afd6b02f81f55b6ffbeebb6
Luis Yauri <david.yauri147@gmail.com>
2019-10-22 08:03:54 -0500

    update angular

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {businnes} from '../../model/business.model';
+import {BusinessService} from '../../services/business.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  listBusiness:businnes[] = []
+  constructor(private businessService:BusinessService) { }
 
   ngOnInit() {
+    this.getBusinessss()
   }
 
+  async getBusinessss() {
+    try {
+      const response:any = await this.businessService.getBusiness().toPromise()
+      this.listBusiness = response.data
+    }catch (e) {
+      console.log(e)
+    }
+  }
 }
